e46eca40f76a6b739252468cc9ca689957950563
Md Sakibul Islam <sakibul.islam0808@gmail.com>
2024-12-15 00:56:44 +0600

    learing tabs and stack

--- app/(tabs)/favorites.tsx
+++ app/(tabs)/favorites.tsx
@@ -1,19 +1,20 @@
-import { Link } from "expo-router";
 import { View, Text, Pressable } from "react-native";
+import React from "react";
+import { Link } from "expo-router";
 
-const Home = () => {
+const Favorites = () => {
   return (
     <View className="flex flex-1 items-center justify-center">
       <Text className="text-center text-2xl font-semibold text-teal-700">
-        Hello form home page
+        Hello form favorites page
       </Text>
-      <Link href={"/favorites"} asChild>
+      <Link href={"/"} asChild>
         <Pressable className="bg-teal-700 px-3 py-2 rounded-lg mt-4">
-          <Text className="text-white text-center">Go to Favorites</Text>
+          <Text className="text-white text-center">Go to Home</Text>
         </Pressable>
       </Link>
     </View>
   );
 };
 
-export default Home;
+export default Favorites;
